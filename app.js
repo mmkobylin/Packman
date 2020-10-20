34be2dcf5ae18fc7fe13308e04c134aebb081398
mmkobylin <mmkobylin@gmail.com>
2020-10-20 16:33:54 +0100

    board created

--- app.js
+++ app.js
@@ -43,6 +43,28 @@ document.addEventListener('DOMContentLoaded', () => {
         1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
         1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-      ]
+      ];
 
-})
+    //drawing the grid
+
+    const squares = [];
+
+      function createBoard() {
+          //adding the layout items to the squares
+          for (let i=0; i < layout.length; i++) {
+              const square = document.createElement('div')
+              grid.appendChild(square)
+              squares.push(square);
+
+            if(layout[i] === 0) {
+                squares[i].classList.add('pac-dot')
+            } else if (layout[i] === 1) {
+            squares[i].classList.add('wall')
+            } else if (layout[i] === 3) {
+            squares[i].classList.add('power-pellet')
+            }
+        }
+    }
+
+createBoard()
+});
